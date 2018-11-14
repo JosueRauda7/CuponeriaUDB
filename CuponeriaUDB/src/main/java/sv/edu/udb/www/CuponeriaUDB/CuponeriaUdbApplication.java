b9abd30ebf7dccfd0fd82d6f150ba9bfd5ca3eb9
JosueRauda7 <30637512+JosueRauda7@users.noreply.github.com>
2018-11-14 16:12:18 -0600

    Inicio del proyecto

---
+++ CuponeriaUDB/src/main/java/sv/edu/udb/www/CuponeriaUDB/CuponeriaUdbApplication.java
@@ -0,0 +1,12 @@
+package sv.edu.udb.www.CuponeriaUDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuponeriaUdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuponeriaUdbApplication.class, args);
+	}
+}
