1905b936f2736f4cea167ee5a7fa058e3abe5ac1
Emerson-Torres <30862610+Emerson-Torres@users.noreply.github.com>
2018-11-17 10:27:13 -0600

    Archivos empresa

---
+++ CuponeriaUDB/src/main/java/sv/edu/udb/www/CuponeriaUDB/configuration/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package sv.edu.udb.www.CuponeriaUDB.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled=true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+	
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+        .authorizeRequests()
+            .antMatchers("/css/", "/js/","/empresa/**").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin().loginPage("/login").defaultSuccessUrl("/editoriales/list")
+            .usernameParameter("usuario").passwordParameter("password")
+            .permitAll();
+
+    }
+
+}
