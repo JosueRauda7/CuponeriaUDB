2d5618158e0f5a9f96684e66e778263bffd6af18
VlRoque <roquealexander7@gmail.com>
2018-11-20 00:03:23 -0600

    Empleado

--- CuponeriaUDB/src/main/java/sv/edu/udb/www/CuponeriaUDB/controllers/EmpleadoController.java
+++ CuponeriaUDB/src/main/java/sv/edu/udb/www/CuponeriaUDB/controllers/EmpleadoController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import sv.edu.udb.www.CuponeriaUDB.entities.Cupones;
 import sv.edu.udb.www.CuponeriaUDB.repositories.EmpleadoRepository;
 
 @RequestMapping("/empleado")
@@ -22,4 +23,19 @@ public class EmpleadoController {
 		model.addAttribute("lista", "Prueba");
 		return "empleado/home";
 	}
+	
+	@GetMapping("/canjear")
+	public String canjearCupon(Model model) {
+		model.addAttribute("cupon", new Cupones());
+		
+		return "empleado/canjearCupon";
+	}
+	
+	/*public String obtenerCupon(Model model) {
+		
+		model.addAttribute(, empleadoRepository.obtenerCupon("OMG1230029312"));
+		
+		return "/canjearCupon";
+	}*/
+	
 }
--- CuponeriaUDB/src/main/java/sv/edu/udb/www/CuponeriaUDB/repositories/EmpleadoRepository.java
+++ CuponeriaUDB/src/main/java/sv/edu/udb/www/CuponeriaUDB/repositories/EmpleadoRepository.java
@@ -4,10 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
+//import sv.edu.udb.www.CuponeriaUDB.entities.Cupones;
 import sv.edu.udb.www.CuponeriaUDB.entities.Empleado;
 
 @Repository("EmpleadoRepository")
@@ -15,5 +14,8 @@ public interface EmpleadoRepository extends JpaRepository<Empleado, String> {
 
 	@Query("SELECT e FROM Empleado e WHERE e.empresas.codigoEmpresa=?1")
 	public abstract List<Empleado> encontrarPorEmpresa(String codigo);
+	
+	
+	//public abstract List<Cupones> obtenerCupon();
 	
 }
