295df5adabd9b9f983723d28840c53ce8f2d9a21
JosueRauda7 <30637512+JosueRauda7@users.noreply.github.com>
2018-11-24 05:07:26 -0600

    commit rauda

--- CuponeriaUDB/src/main/java/sv/edu/udb/www/CuponeriaUDB/repositories/ClienteRepository.java
+++ CuponeriaUDB/src/main/java/sv/edu/udb/www/CuponeriaUDB/repositories/ClienteRepository.java
@@ -1,14 +1,4 @@
-package sv.edu.udb.www.CuponeriaUDB.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import sv.edu.udb.www.CuponeriaUDB.entities.Clientes;
-
-@Repository("ClienteRepository")
-public interface ClienteRepository extends JpaRepository<Clientes,Integer> {
-
-}
 package sv.edu.udb.www.CuponeriaUDB.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
--- CuponeriaUDB/src/main/java/sv/edu/udb/www/CuponeriaUDB/repositories/OfertasRepository.java
+++ CuponeriaUDB/src/main/java/sv/edu/udb/www/CuponeriaUDB/repositories/OfertasRepository.java
@@ -1,6 +1,6 @@
 package sv.edu.udb.www.CuponeriaUDB.repositories;
 
-<<<<<<< HEAD
+
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,15 +13,4 @@ import sv.edu.udb.www.CuponeriaUDB.entities.Ofertas;
 public interface OfertasRepository extends JpaRepository<Ofertas, Integer> {
 	
 	public abstract Ofertas findByIdOferta(Integer id);
-=======
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import sv.edu.udb.www.CuponeriaUDB.entities.Ofertas;
-
-
-@Repository("OfertasRepository")
-public interface OfertasRepository extends JpaRepository<Ofertas, String>{
-	
->>>>>>> branch 'master' of https://github.com/JosueRauda7/CuponeriaUDB.git
 }
